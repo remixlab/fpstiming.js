8fbc9ec76514b92ac5b3183253bd19a7a440fe51
nakednous <nakednous@gmail.com>
2013-09-21 18:06:40 -0500

    updating readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-fpstiming
+FPSTiming
 =========
+
+fps-based timing.
+
+A sequential single-threaded timer on top of which Proscene animations 
+and timing routines are built.
--- src/remixlab/fpstiming/TimingHandler.java
+++ src/remixlab/fpstiming/TimingHandler.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * FPSTiming (version 0.9.50)
+ * Copyright (c) 2013 Jean Pierre Charalambos.
+ * @author Jean Pierre Charalambos
+ * https://github.com/remixlab
+ * 
+ * All rights reserved. Library that eases the creation of interactive
+ * scenes released under the terms of the GNU Public License v3.0
+ * which available at http://www.gnu.org/licenses/gpl.html
+ ******************************************************************************/
 package remixlab.fpstiming;
 
 import java.util.ArrayList;
